8bbbdec1c32333247e55e73c16b1f179304faad6
Shawn Dempsey <sndempsey@gmail.com>
2013-07-16 15:48:17 -0700

    add spec_helper and spec rake task

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+require 'bundler'
+Bundler.setup
+
+SPEC_ROOT = File.dirname(__FILE__)
+
+RSpec.configure do |config|
+  config.mock_with :rspec
+end
+
+Dir[File.join(SPEC_ROOT, '../lib/**/*.rb')].each{ |file| require file }
+
+Dir[File.join(SPEC_ROOT, 'support', '*.rb')].each{ |file| require file }
+Dir[File.join(SPEC_ROOT, 'support', 'shared_examples', '*.rb')].each{ |file| require file }
+

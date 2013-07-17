d4c3def2968a76f8330b11df5b39c28aad32415f
Shawn Dempsey <sndempsey@gmail.com>
2013-07-16 17:48:33 -0700

    Add toggle to light

--- spec/hue_conference/light_spec.rb
+++ spec/hue_conference/light_spec.rb
@@ -23,7 +23,7 @@ describe "HueConference::Light" do
     @light.client.should == mock_client
   end
 
-  describe "on/off" do
+  describe "turn on/off" do
     let(:mock_client) { mock }
 
     before do
@@ -32,12 +32,78 @@ describe "HueConference::Light" do
 
     it "should turn on the light" do
       mock_client.should_receive("put").with("/lights/1/state", on: true)
+      @light.stub(:on?) { false }
       @light.turn_on
     end
 
     it "should turn off the light" do
+      @light.stub(:on?) { true }
       mock_client.should_receive("put").with("/lights/1/state", on: false)
       @light.turn_off
     end
   end
+
+  describe "#sync_state" do
+    let(:mock_client) { mock }
+
+    let(:light_state) {
+      {
+        "state"=> {
+          "hue"=> 50000,
+          "on"=> false,
+          "effect"=> "none",
+          "alert"=> "none",
+          "bri"=> 200,
+          "sat"=> 200,
+          "ct"=> 500,
+          "xy"=> [0.5, 0.5],
+          "reachable"=> true,
+          "colormode"=> "hs"
+        }
+      }
+    }
+    let(:response) { mock_response = mock
+                     mock_response.stub(:data).and_return(light_state)
+                     mock_response
+    }
+
+
+    before do
+      @light.client = mock_client
+      mock_client.stub(:get).and_return(response)
+    end
+
+    it "should fetch the state" do
+      mock_client.should_receive("get").with("/lights/1")
+      @light.sync!
+    end
+
+    it "should set the 'on' state" do
+      @light.sync!
+      @light.on?.should be_false
+    end
+  end
+
+  describe "#on?" do
+    it "should know if it's on" do
+      @light.on?.should == true
+    end
+  end
+
+  describe "#toggle" do
+    it "should turn on if off" do
+      @light.should_receive(:turn_on)
+      @light.should_not_receive(:turn_off)
+      @light.stub(:on?) { false }
+      @light.toggle
+    end
+
+    it "should turn off if on" do
+      @light.should_receive(:turn_off)
+      @light.should_not_receive(:turn_on)
+      @light.stub(:on?) { true }
+      @light.toggle
+    end
+  end
 end
+

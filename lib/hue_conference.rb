eb84427c7729827fcaa9886e31d1a4f24cf4a6b5
Shawn Dempsey <sndempsey@gmail.com>
2013-07-16 17:14:51 -0700

    Add example

--- lib/hue_conference.rb
+++ lib/hue_conference.rb
@@ -1,5 +1,7 @@
 require "hue_conference/version"
 require "hue_conference/room"
+require "hue_conference/light"
+require "hue_conference/light_manifest"
 
 module HueConference
   # Your code goes here...

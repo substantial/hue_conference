e7786db408ddbd3a11dfc49c18da1e4c86957600
Shawn Dempsey <sndempsey@gmail.com>
2013-07-16 17:48:33 -0700

    Add toggle to light

--- lib/hue_conference/light.rb
+++ lib/hue_conference/light.rb
@@ -5,14 +5,39 @@ class HueConference::Light
   def initialize(id, properties = {})
     @id = id
     @name = properties['name']
+    @on = true
   end
 
   def turn_on
+   unless on?
     @client.put("/lights/#{id}/state", on: true)
+    @on = true
+   end
   end
 
   def turn_off
+   if on?
     @client.put("/lights/#{id}/state", on: false)
+    @on = false
+   end
+  end
+
+  def on?
+    @on
+  end
+
+  def sync!
+    light_state = @client.get("/lights/#{id}").data['state']
+
+    @on = light_state['on']
+  end
+
+  def toggle
+    if on?
+      turn_off
+    else
+      turn_on
+    end
   end
 
 end

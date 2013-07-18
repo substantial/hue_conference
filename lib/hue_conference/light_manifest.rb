1dd6d0211464f817599dd959980da5030fd40601
Shawn Dempsey <sndempsey@gmail.com>
2013-07-18 16:27:38 -0700

    Update light manifest

--- lib/hue_conference/light_manifest.rb
+++ lib/hue_conference/light_manifest.rb
@@ -3,7 +3,7 @@ require 'hue_conference/light'
 
 class HueConference::LightManifest
 
-  attr_reader :client, :lights
+  attr_reader :client
 
   def initialize(ruhue_client)
     @client = ruhue_client
@@ -11,6 +11,7 @@ class HueConference::LightManifest
   end
 
   def build_manifest
+    @lights.clear
     lights_hash = @client.get("/lights").data
     lights_hash.each_pair do |light_id, light_properties|
       light = HueConference::Light.new(light_id, light_properties)
@@ -19,4 +20,20 @@ class HueConference::LightManifest
     end
   end
 
+  def lights
+    build_manifest if @lights.empty?
+    @lights
+  end
+
+  def lights!
+    build_manifest
+    @lights
+  end
+
+  def find_light(light_name)
+    lights.select do |light|
+      light.name == light_name
+    end.first
+  end
 end
+
--- spec/lib/hue_conference/light_manifest_spec.rb
+++ spec/lib/hue_conference/light_manifest_spec.rb
@@ -12,6 +12,7 @@ describe HueConference::LightManifest do
     hue = double
     ruhue_client = Ruhue::Client.new(hue, "foousername")
     manifest = HueConference::LightManifest.new(ruhue_client)
+    manifest.stub(:build_manifest)
     manifest.lights.should be_a Array
   end
 
@@ -42,10 +43,64 @@ describe HueConference::LightManifest do
       manifest.build_manifest
     end
 
+    it "should clear the existings lights" do
+      ruhue_client.stub_chain(:get, :data) { {} }
+
+      manifest.instance_variable_set(:@lights, ['foo'])
+      manifest.build_manifest
+      manifest.instance_variable_get(:@lights).should be_empty
+    end
+
     it "should instantiate lights" do
       HueConference::Light.should_receive(:new).with("1", response_data["1"])
       HueConference::Light.should_receive(:new).with("2", response_data["2"])
       manifest.build_manifest
+    end
+  end
+
+  describe "#lights" do
+    let(:manifest) { HueConference::LightManifest.new(double) }
+
+    before do
+      manifest.stub(:build_manifest)
+    end
+
+    it "should build the manifest if lights are empty" do
+      manifest.should_receive(:build_manifest)
+      manifest.lights
+    end
+
+    it "shouldn't build the manifest if lights aren't empty" do
+      manifest.instance_variable_set(:@lights, ['foo'])
+      manifest.should_not_receive(:build_manifest)
+      manifest.lights
+    end
+  end
+
+  describe "#lights!" do
+    let(:manifest) { HueConference::LightManifest.new(double) }
+
+    before do
+      manifest.stub(:build_manifest)
+    end
+
+    it "should force build manifest" do
+      manifest.instance_variable_set(:@lights, ['foo'])
+      manifest.should_receive(:build_manifest)
+      manifest.lights!
+    end
+  end
+
+  describe "#find_light" do
+    let(:manifest) { HueConference::LightManifest.new(double) }
+    let(:foo_light) { double(name: "foo_light") }
+
+    before do
+      manifest.stub(:lights) { [foo_light] }
+    end
+
+    it "should return the light matching the given name" do
+      manifest.find_light("foo_light").should == foo_light
     end
   end
 end

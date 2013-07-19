92839cc93848d34c50bcff6beda9b0c0a7a4dd3c
Shawn Dempsey <sndempsey@gmail.com>
2013-07-18 17:36:11 -0700

    Add #next_event to Calendar

--- lib/hue_conference/calendar.rb
+++ lib/hue_conference/calendar.rb
@@ -1,11 +1,25 @@
+require 'hue_conference/event'
+
 module HueConference
   class Calendar
     require 'google/api_client'
 
-    attr_reader :id
+    attr_reader :id, :events
 
     def initialize(calendar_id)
       @id = calendar_id
+      @events = []
+    end
+
+    def build_events(google_events_response)
+      @events.clear
+      google_events_response.each do |event_hash|
+        @events << HueConference::Event.new(event_hash)
+      end
+    end
+
+    def next_event
+      @events.sort.first
     end
   end
 end

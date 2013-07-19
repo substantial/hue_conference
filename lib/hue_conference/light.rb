c7337fbb137cecebba8e7151d6596af0133f2d75
Shawn Dempsey <sndempsey@gmail.com>
2013-07-19 15:35:55 -0700

    update light. Get state from response

--- lib/hue_conference/light.rb
+++ lib/hue_conference/light.rb
@@ -1,42 +1,70 @@
-class HueConference::Light
-  attr_reader :name, :id
-  attr_accessor :client, :location
+module HueConference
 
-  def initialize(id, properties = {})
-    @id = id
-    @name = properties['name']
-    @on = true
-  end
+  class FloatOutOfRange < Exception; end
 
-  def turn_on
-   unless on?
-    @client.put("/lights/#{id}/state", on: true)
-    @on = true
-   end
-  end
+  class Light
+    attr_reader :name, :id
+    attr_accessor :client, :location
 
-  def turn_off
-   if on?
-    @client.put("/lights/#{id}/state", on: false)
-    @on = false
-   end
-  end
+    STATE_PROPERTIES = %w[on hue bri sat ct alert effect transitiontime]
+    MAX_BRIGHTNESS = 255
+    MAX_SATURATION = 255
 
-  def on?
-    @on
-  end
+    def initialize(id, properties = {})
+      @id = id
+      @name = properties['name']
+    end
 
-  def sync!
-    light_state = @client.get("/lights/#{id}").data['state']
+    def turn_on
+      update_state(on: true) unless @on
+    end
 
-    @on = light_state['on']
-  end
+    def turn_off
+      update_state(on: false) if @on
+    end
 
-  def toggle
-    if on?
-      turn_off
-    else
-      turn_on
+    def sync!
+      light_state = @client.get("/lights/#{id}").data['state']
+      STATE_PROPERTIES.each do |property|
+        instance_variable_set("@#{property}".to_sym, light_state[property])
+      end
+    end
+
+    def toggle
+      @on ? turn_off : turn_on
+    end
+
+    def brightness(factor)
+      validate_factor(factor)
+      update_state(bri: (MAX_BRIGHTNESS * factor).round )
+    end
+
+    def saturation(factor)
+      validate_factor(factor)
+      update_state(sat: (MAX_SATURATION * factor).round )
+    end
+
+    private
+
+    def validate_factor(factor)
+      invalid = (factor < 0.0 || factor > 1.0)
+      raise FloatOutOfRange, "Number must be between 0.0 to 1.0" if invalid
+    end
+
+    def update_state(new_state={})
+      result = @client.put("/lights/#{id}/state", new_state)
+
+      result.data.each do |state|
+        if state.has_key?('success')
+          state['success'].each do |state_property|
+            property_name = state_property.first.match('[a-zA-z]*$').to_s
+            property_sym = "@#{property_name}".to_sym
+            property_value = state_property.last
+
+            instance_variable_set(property_sym, property_value)
+          end
+        end
+      end
     end
   end
 

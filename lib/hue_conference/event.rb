2e02fefccba7fab051605882fb2ec8bfb78d2d81
Shawn Dempsey <sndempsey@gmail.com>
2013-07-24 12:02:08 -0700

    add Date & DateTime parsing to Event

--- lib/hue_conference/event.rb
+++ lib/hue_conference/event.rb
@@ -5,11 +5,24 @@ class HueConference::Event
 
   def initialize(google_events_response)
     @name = google_events_response.summary
-    @start_date = DateTime.parse(google_events_response.start.dateTime.to_s)
-    @end_date = DateTime.parse(google_events_response.end.dateTime.to_s)
+
+    @start_date = parse_date_time(google_events_response.start)
+    @end_date = parse_date_time(google_events_response.end)
   end
 
   def <=>(other)
-    other.start_date <=> @start_date
+     @start_date <=> other.start_date
+  end
+
+  def started?
+    @start_date < DateTime.now
+  end
+
+  private
+
+  def parse_date_time(response_date)
+    date_time = response_date.date ? response_date.date : response_date.dateTime
+
+    DateTime.parse(date_time.to_s)
   end
 end
--- spec/lib/hue_conference/event_spec.rb
+++ spec/lib/hue_conference/event_spec.rb
@@ -1,27 +1,41 @@
 require 'spec_helper'
 
 describe "HueConference::Event" do
-  let(:start_time) { Time.now }
-  let(:end_time) { Time.now }
-  let(:google_event_object) { double(start: double(dateTime: start_time),
-                                     end: double(dateTime: end_time),
+
+  let(:start_time) { DateTime.now }
+  let(:end_time) { DateTime.now }
+  let(:google_event_object) { double(start: double(dateTime: start_time, date: nil),
+                                     end: double(dateTime: end_time, date: nil),
                                      summary: "Event Name"
                                     )
   }
+  let(:event) { HueConference::Event.new(google_event_object) }
 
-  it "should should set the start date" do
-    event = HueConference::Event.new(google_event_object)
-    event.start_date.should == DateTime.parse(start_time.to_s)
+  before do
+    DateTime.stub(:now) { DateTime.new(2010, 1, 1, 10) }
   end
 
-  it "should should set the end date" do
-    event = HueConference::Event.new(google_event_object)
-    event.end_date.should == DateTime.parse(end_time.to_s)
+  describe "#initialize" do
+    it "should should set the start date" do
+      event.start_date.should == DateTime.parse(start_time.to_s)
+    end
+
+    it "should should set the end date" do
+      event.end_date.should == DateTime.parse(end_time.to_s)
+    end
+
+    it "should have an event summary/name" do
+      event.name.should == "Event Name"
+    end
+
   end
 
-  it "should have an event summary/name" do
-    event = HueConference::Event.new(google_event_object)
-    event.name.should == "Event Name"
+  describe "#started?" do
+    let(:start_time) { DateTime.new(2010, 1, 1, 9) }
+
+    it "should be true if event has started" do
+      event.started?.should == true
+    end
   end
 
   describe "sorting, <==>" do
@@ -36,14 +50,13 @@ describe "HueConference::Event" do
     end
 
     it "should know how to compare each other" do
-      old_event.should > new_event
-      old_event.should > newest_event
+      old_event.should < new_event
+      old_event.should < newest_event
     end
 
     it "should be sortable" do
-      [new_event, old_event, newest_event].sort.should == [newest_event, new_event, old_event]
+      [new_event, old_event, newest_event].sort.should == [old_event, new_event, newest_event]
     end
   end
-
 end
 

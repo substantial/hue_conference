83c3b63756eee1269fd3c4c1c863c5e985387e83
Shawn Dempsey <sndempsey@gmail.com>
2013-07-30 14:13:13 -0700

    room builder doesn't build events anymore

--- spec/lib/hue_conference/room_builder_spec.rb
+++ spec/lib/hue_conference/room_builder_spec.rb
@@ -1,6 +1,7 @@
 require "spec_helper"
 
 describe "HueConference::RoomBuilder" do
+
   let(:light_hash) { {"name" => "light_name", "location" => "some location"} }
   let(:rooms_config) {
     [
@@ -9,10 +10,12 @@ describe "HueConference::RoomBuilder" do
       "lights" => [ light_hash ]
     ]
   }
-  let(:light_manifest) { double(is_a?: true) }
+  let(:light) { double.as_null_object }
+  let(:light_manifest) { double(is_a?: true, find_light: light) }
   let(:google_agent) { double(is_a?: true) }
 
   describe "#initialize" do
+
     it "should require a config hash" do
       expect { HueConference::RoomBuilder.new }.to raise_error ArgumentError
     end
@@ -36,30 +39,23 @@ describe "HueConference::RoomBuilder" do
 
   describe "#build" do
 
-    let(:light) { double.as_null_object }
-    let(:light_manifest) { double(is_a?: true, find_light: light) }
-    let(:google_agent) { double(is_a?: true, calendar_events: 'some calendar events') }
-    let(:builder) { HueConference::RoomBuilder.new(rooms_config, light_manifest, google_agent) }
     let(:room) { double.as_null_object }
-    let(:calendar) { double.as_null_object }
+    let(:calendar) { double }
+    let(:schedule) { double }
+    let(:builder) { HueConference::RoomBuilder.new(rooms_config, light_manifest, google_agent) }
 
     before do
-      HueConference::Calendar.stub(new: calendar)
       HueConference::Room.stub(new: room)
-    end
-
-    it "should create a calendar for each room" do
-      HueConference::Calendar.should_receive(:new).with(rooms_config.first["calendar_id"])
-      builder.build
-    end
-
-    it "should build events for a calendar" do
-      calendar.should_receive(:build_events).with('some calendar events')
-      builder.build
+      HueConference::Calendar.stub(new: calendar)
     end
 
     it "should create a room" do
       HueConference::Room.should_receive(:new).with(rooms_config.first)
+      builder.build
+    end
+
+    it "should create a calendar" do
+      HueConference::Calendar.should_receive(:new)
       builder.build
     end
 
